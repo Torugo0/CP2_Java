ce54402f0b886a151486a4643d371b94b223428a
Vitor Hugo <vitorh565.vh@gmal.com>
2024-04-15 11:15:49 -0300

    Finish CP

---
+++ src/br/com/fiap/bean/Aluno.java
@@ -0,0 +1,72 @@
+// Vitor Hugo Gonçalves Rodrigues - RM: 97758
+// Leticia Cristina Gandarez Resina - RM: 98069
+
+package br.com.fiap.bean;
+
+import java.time.LocalDate;
+import java.time.Period;
+import javax.swing.JOptionPane;
+
+public class Aluno {
+    private int rm;
+    private String nome;
+    private LocalDate dataDeNascimento;
+
+    public Aluno(){} //Criação de constructors vazio e com passagem de parametros
+
+    public Aluno(String nome, int rm, LocalDate dataDeNascimento){
+        this.nome = nome;
+        setRm(rm); // Não coloco um this.rm = rm; porque ele precisa passar pela verificação antes de definir o rm.
+        setDataDeNascimento(dataDeNascimento); // A mesma coisa serve para a data de nascimento.
+    }
+
+    public int getRm() {
+        return rm;
+    }
+    public void setRm(int rm) {
+        try {
+            if (rm >= 97001 && rm <= 551999) {
+                this.rm = rm;
+            } else {
+                throw new Exception("RM INVÁLIDO!");
+            }
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, e.getMessage());
+            System.exit(0);
+        }
+
+    }
+
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public LocalDate getDataDeNascimento() {
+        return dataDeNascimento;
+    }
+    public void setDataDeNascimento(LocalDate dataDeNascimento) {
+        LocalDate minimo = LocalDate.parse("1899-12-31");
+        LocalDate anoAtual = LocalDate.now(); // Pega a data atual do sistema
+        try {
+            if (dataDeNascimento.isAfter(minimo) && dataDeNascimento.isBefore(anoAtual)) {
+                this.dataDeNascimento = dataDeNascimento; // Caso esteja tudo dentro da regra de negocios ele setta a data.
+            } else {
+                throw new Exception("Data fora da faixa permitida (1900-01-01" + " - " + anoAtual + ")");
+                
+            }
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, e.getMessage());
+            System.exit(0);
+        }}
+
+        public String calculaIdadeCompleta(LocalDate dataAtual) {
+            Period periodo = Period.between(dataDeNascimento, dataAtual);
+            
+            String idadeCompleta = periodo.getYears() + " anos, " + periodo.getMonths() + " meses e " + periodo.getDays() + " dias";
+
+            return idadeCompleta;
+        }
+}
---
+++ src/br/com/fiap/main/UsaAluno.java
@@ -0,0 +1,67 @@
+// Vitor Hugo Gonçalves Rodrigues - RM: 97758
+// Leticia Cristina Gandarez Resina - RM: 98069
+
+package br.com.fiap.main;
+
+import java.time.LocalDate;
+import javax.swing.JOptionPane;
+import br.com.fiap.bean.Aluno;
+
+public class UsaAluno {
+    public static void main(String[] args) {
+        String nome, guardaData, aux;
+        int rm;
+        LocalDate dataNascimento;
+        LocalDate dataAtual = LocalDate.now();
+
+        try {
+            // Declaração, instanciação e exibição de dois objetos com constructor vazio.
+            Aluno vitor = new Aluno();
+            Aluno leticia = new Aluno();
+
+            vitor.setNome("Vitor Hugo Gonçalves Rodrigues");
+            vitor.setRm(97758);
+            vitor.setDataDeNascimento(LocalDate.parse("2005-03-08")); 
+            
+            leticia.setNome("Leticia Cristina Gandarez Resina");
+            leticia.setRm(98069);
+            leticia.setDataDeNascimento(LocalDate.parse("2004-08-04"));
+
+            JOptionPane.showMessageDialog(null, "RM: " + vitor.getRm() + " - NOME: " + vitor.getNome() + " - IDADE COMPLETA: " + vitor.calculaIdadeCompleta(dataAtual));
+            JOptionPane.showMessageDialog(null, "RM: " + leticia.getRm() + " - NOME: " + leticia.getNome() + " - Idade completa: " + leticia.calculaIdadeCompleta(dataAtual));      
+            
+            // Declaração, instanciação e exibição de dois objetos com constructor com passagem de parametros.
+            
+            // Pessoa 1
+            nome = JOptionPane.showInputDialog("Digite seu nome:");
+            rm = Integer.parseInt(JOptionPane.showInputDialog("Informe seu RM:"));
+
+            aux = JOptionPane.showInputDialog("Informe sua data de nascimento:");
+            guardaData = aux.substring(6,10); //2024
+            guardaData += "-" + aux.substring(3, 5); // 2024-11
+            guardaData += "-" + aux.substring(0, 2); // 2024-11-04
+
+            dataNascimento = LocalDate.parse(guardaData);
+            Aluno p1 = new Aluno(nome, rm, dataNascimento);
+
+            // Pessoa 2
+            nome = JOptionPane.showInputDialog("Digite seu nome:");
+            rm = Integer.parseInt(JOptionPane.showInputDialog("Informe seu RM:"));
+
+            aux = JOptionPane.showInputDialog("Informe sua data de nascimento:");
+            guardaData = aux.substring(6,10); //2024
+            guardaData += "-" + aux.substring(3, 5); // 2024-11
+            guardaData += "-" + aux.substring(0, 2); // 2024-11-04
+
+            dataNascimento = LocalDate.parse(guardaData);
+            Aluno p2 = new Aluno(nome, rm, dataNascimento);
+
+            // Exibindo informações
+            JOptionPane.showMessageDialog(null, "RM: " + p1.getRm() + " - NOME: " + p1.getNome() + " - IDADE COMPLETA: " + p1.calculaIdadeCompleta(dataAtual));
+            JOptionPane.showMessageDialog(null, "RM: " + p2.getRm() + " - NOME: " + p2.getNome() + " - Idade completa: " + p2.calculaIdadeCompleta(dataAtual));  
+
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, e.getMessage());
+        }
+    }
+}
